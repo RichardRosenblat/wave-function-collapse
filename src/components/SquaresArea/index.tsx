8040ab70176a75f8205ed556d0c49aa712028d6a
Richard Rosenblat <rosenblatr@gmail.com>
2023-02-15 17:36:35 -0300

    add colors

--- src/components/SquaresArea/index.tsx
+++ src/components/SquaresArea/index.tsx
@@ -1,14 +1,21 @@
 import Styles from "./SquaresArea.module.scss";
 
+type xOptions = "right" | "middle" | "left";
+
+type yOptions = "top" | "center" | "bottom";
+
+type colorOptions = "color-1" | "color-2";
 interface props {
 	id?: string;
-	x: "right" | "middle" | "left";
-	y: "top" | "center" | "bottom";
+	x: xOptions;
+	y: yOptions;
 }
 
-const SquaresArea = ({ id }: props) => {
+const SquaresArea = ({ id, x, y }: props) => {
+	const color = getBackgroundColor(x, y);
 	return (
-		<div>
+		<>
+		<div className={Styles[color]}>
 			<div className={Styles.line}>
 				<div>{id}1</div>
 				<div>{id}2</div>
@@ -27,7 +34,27 @@ const SquaresArea = ({ id }: props) => {
 				<div>{id}9</div>
 			</div>
 		</div>
+		<div className={Styles['between-lines']}></div>
+		</>
 	);
+
+	function getBackgroundColor(x: xOptions, y: yOptions): colorOptions {
+		const map = {
+			right: 1,
+			middle: 2,
+			left: 3,
+
+			top: 1,
+			center: 2,
+			bottom: 3,
+		};
+
+		if (Math.abs(map[x] - map[y]) === 1) {
+			return "color-1";
+		}
+
+		return "color-2";
+	}
 };
 
 export default SquaresArea;
