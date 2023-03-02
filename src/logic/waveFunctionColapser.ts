a4ce359e9765eeb7f64337e2efa5af5c61418973
Richard Rosenblat <rosenblatr@gmail.com>
2023-03-02 19:00:34 -0300

    finish collapse cell function

--- src/logic/waveFunctionColapser.ts
+++ src/logic/waveFunctionColapser.ts
@@ -13,23 +13,52 @@ export function collapseCell(collapsedCoords: number[], colapsedInto: number, bo
 }
 
 function updateBoardPossibleStates(collapsedCoords: number[], valueCollapsedInto: number, board: cell[][][][]) {
-	const [Y, X] = collapsedCoords;
-	const cellArea = board[Y][X];
+	const [Y, X, y, x] = collapsedCoords;
 
 	updateAreaStates();
+	updateLineStates("horizontal");
+	updateLineStates("vertical");
 
 	function updateAreaStates() {
+		const cellArea = board[Y][X];
+
 		cellArea.forEach((row, cellY) => {
 			row.forEach((currentCell, cellX) => {
 				if (!currentCell.hasCollapsed) {
-					currentCell.possibleStates.delete(valueCollapsedInto);
-
-					if (currentCell.possibleStates.size === 1) {
-						const remainingState = Array.from(currentCell.possibleStates.values())[0];
-						collapseCell([Y, X, cellY, cellX], remainingState, board);
-					}
+					removeStateOrCollapse([Y, X, cellY, cellX]);
 				}
 			});
 		});
+	}
+	function updateLineStates(direction: "vertical" | "horizontal") {
+		const collapsedCellRef = direction === "vertical" ? Y : X;
+
+		for (let areaCursor = 0; areaCursor < 3; areaCursor++) {
+			if (areaCursor === collapsedCellRef) {
+				continue;
+			}
+
+			for (let cellCursor = 0; cellCursor < 3; cellCursor++) {
+				if (direction === "vertical") {
+					removeStateOrCollapse([areaCursor, X, cellCursor, x]);
+				} else {
+					removeStateOrCollapse([Y, areaCursor, y, cellCursor]);
+				}
+			}
+		}
+	}
+
+	function removeStateOrCollapse(coordsToCheck: number[]) {
+		const [Y, X, y, x] = coordsToCheck;
+		const currentCell = board[Y][X][y][x];
+
+		if (!currentCell.hasCollapsed) {
+			currentCell.possibleStates.delete(valueCollapsedInto);
+
+			if (currentCell.possibleStates.size === 1) {
+				const remainingState = Array.from(currentCell.possibleStates.values())[0];
+				collapseCell([Y, X, y, x], remainingState, board);
+			}
+		}
 	}
 }
