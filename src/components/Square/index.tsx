a7a88586307f1fe28a42319ba166279904a83196
Richard Rosenblat <rosenblatr@gmail.com>
2023-03-28 19:22:28 -0300

    add classnames

--- src/components/Square/index.tsx
+++ src/components/Square/index.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import { useCollapseMenu } from "../../hooks/useCollapseMenu";
 import { cell } from "../../types/cell";
 import Styles from './Square.module.scss'
@@ -12,15 +13,18 @@ const Square = ({ children: cell }: props) => {
 	const firstValueFromCell = cell.possibleStates.values().next().value;
 
 	const handleCellClick = () => { open(cell) };
+
 	return (
-		<div 
-			onClick={handleCellClick} 
-			className={Styles[
-				'cell' + (cell.hasCollapsed ? '_solved' : '') + 
-				(selectedCell?.id === cell.id && !cell.hasCollapsed ? '_selected' : '')
-				]
+		<div
+			onClick={handleCellClick}
+			className={
+				classNames({
+					[Styles.cell]: true,
+					[Styles['cell--solved']]: cell.hasCollapsed,
+					[Styles['cell--selected']]: selectedCell?.id === cell.id && !cell.hasCollapsed,
+				})
 			}
-			>
+		>
 			{cell.hasCollapsed ? firstValueFromCell : "?"}
 		</div>
 	);
