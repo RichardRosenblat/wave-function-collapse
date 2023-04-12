e73465e618573ae43a88bfde039026e646e576f1
Richard Rosenblat <rosenblatr@gmail.com>
2023-04-12 20:40:42 -0300

    add borders, title and change fonts

--- src/components/Square/index.tsx
+++ src/components/Square/index.tsx
@@ -2,6 +2,7 @@ import classNames from "classnames";
 import { useCollapseMenu } from "../../hooks/useCollapseMenu";
 import { cell } from "../../types/cell";
 import Styles from './Square.module.scss'
+import { getCellCoordinatesFromId } from "../../util/getCellCoordinatesFromId";
 
 interface props {
 	children: cell;
@@ -11,8 +12,10 @@ const Square = ({ children: cell }: props) => {
 	const { open, selectedCell } = useCollapseMenu();
 
 	const firstValueFromCell = cell.possibleStates.values().next().value;
-
+	const [, , y, x] = getCellCoordinatesFromId(cell.id)
 	const handleCellClick = () => { open(cell) };
+
+
 
 	return (
 		<div
@@ -22,6 +25,10 @@ const Square = ({ children: cell }: props) => {
 					[Styles.cell]: true,
 					[Styles['cell--solved']]: cell.hasCollapsed,
 					[Styles['cell--selected']]: selectedCell?.id === cell.id && !cell.hasCollapsed,
+					[Styles['cell__thick-border-right']]: x === 2,
+					[Styles['cell__thick-border-bottom']]: y === 2,
+					[Styles['cell__thick-border-left']]: x === 0,
+					[Styles['cell__thick-border-top']]: y === 0,
 				})
 			}
 		>
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,21 @@ import "normalize.css";
 import "./index.css";
 import Sudoku from "./pages/Sudoku";
 import { RecoilRoot } from "recoil";
+import { Typography } from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
 	<React.StrictMode>
 		<RecoilRoot>
+			<Typography variant="h1" style={{
+				textAlign: "center",
+				fontSize: "2rem",
+				margin: "1rem",
+				fontWeight: "bold",
+			}} >
+				Wave Function Collapse
+			</Typography>
+
 			<Sudoku />
 		</RecoilRoot>
 	</React.StrictMode>
